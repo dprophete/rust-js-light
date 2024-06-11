024ba01968105acf9e3f022a102e8a14aa882272
Didier Prophete <didier.prophete@gmail.com>
2024-06-11 09:07:04 -0700

    usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::{CommandFactory, Parser as _};
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
@@ -6,14 +8,48 @@ extern crate pest_derive;
 #[grammar = "parser/lang.pest"]
 pub struct LangParser;
 
-use std::fs;
+#[derive(clap::Parser, Debug)]
+#[command(about = "A tiny js-like interpreter", long_about = None)]
+pub struct MainArgs {
+    #[clap(long)]
+    file: Option<String>,
+    #[clap(long)]
+    expr: Option<String>,
+}
+
+use std::{fs, process};
 
 mod parser;
 mod runner;
 
 fn main() {
-    let file_content = fs::read_to_string("resources/ex1.js_new").expect("cannot read file");
-    let prg = parser::parse_prg(&file_content).expect("cannot parse file");
+    let args = MainArgs::parse();
+
+    // expr
+    if let Some(expr) = &args.expr {
+        run_prg(expr);
+        process::exit(0)
+    } else
+    // file
+    if let Some(file) = &args.file {
+        match fs::read_to_string(file) {
+            Ok(content) => {
+                run_prg(&content);
+                process::exit(0)
+            }
+            Err(e) => {
+                eprintln!("Error reading file {}: {}", file, e);
+                process::exit(1)
+            }
+        }
+    }
+
+    MainArgs::command().print_help().unwrap();
+    process::exit(0)
+}
+
+fn run_prg(content: &str) {
+    let prg = parser::parse_prg(&content).expect("parsing errro");
     println!("parsed prg:\n{}", prg);
 
     println!("executing prg");
